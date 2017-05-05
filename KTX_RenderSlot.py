8b04b23aebad74b046c224341589c8b1a6efd6b1
kostex <koelooptiemanna@gmail.com>
2017-05-05 19:39:44 +0200

    KTX_Renderslot added handler to update automatically

--- KTX_RenderSlot.py
+++ KTX_RenderSlot.py
@@ -1,7 +1,25 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 bl_info = {
     "name": "KTX RenderSlot",
     "author": "Roel Koster, @koelooptiemanna, irc:kostex",
-    "version": (1, 1),
+    "version": (1, 2),
     "blender": (2, 7, 0),
     "location": "Properties Editor > Render > Render",
     "category": "Render"}
@@ -9,6 +27,7 @@ bl_info = {
 import bpy,os
 from bpy.props import IntProperty
 from sys import platform
+from bpy.app.handlers import persistent
 
 nullpath = '/nul' if platform == 'win32' else '/dev/null'
 
@@ -18,7 +37,7 @@ class SlotBuffer():
 class KTX_RenderSlot(bpy.types.Operator):
     bl_label = "Select Render Slot"
     bl_idname = "ktx.renderslot"
-    bl_description = "Select Render Slot"
+    bl_description = "Select Render Slot ('>' denotes active slot)"
 
     number = IntProperty()
     
@@ -48,35 +67,42 @@ class KTX_CheckSlots(bpy.types.Operator):
         img.render_slots.active_index= active
         return {'FINISHED'}
 
-
 def ui(self, context):
         layout = self.layout
         row = layout.row(align=True)
         row.alignment='LEFT'
         try:
-            active=bpy.data.images['Render Result'].render_slots.active_index+1
-            row.operator('ktx.checkslots',text='',icon='QUESTION')
-            row.label('Current Render Slot: ' + str(active))
+            active=bpy.data.images['Render Result'].render_slots.active_index
             row = layout.row(align=True)
             row.alignment='EXPAND'
             i = 0
             for i in range(8):
-                if bpy.context.scene.ktx_occupied_render_slots.data[i] == '1':
-                    row.operator('ktx.renderslot', text=str(i+1)+'*').number=i
+                if i==active:
+                    label='>'+str(i+1)
                 else:
-                    row.operator('ktx.renderslot', text=str(i+1)).number=i
+                    label=str(i+1)
+                if bpy.context.scene.ktx_occupied_render_slots.data[i] == '1':
+                    row.operator('ktx.renderslot', text=label+'*').number=i
+                else:
+                    row.operator('ktx.renderslot', text=label).number=i
         except:
             row.label('No Render Slots available yet')
+
+@persistent
+def ktx_render_handler(scene):
+    bpy.ops.ktx.checkslots()
 
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.RENDER_PT_render.prepend(ui)
     bpy.types.Scene.ktx_occupied_render_slots = SlotBuffer()
+    bpy.app.handlers.render_post.append(ktx_render_handler)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
     bpy.types.RENDER_PT_render.remove(ui)
     del bpy.types.Scene.ktx_occupied_render_slots
+    bpy.app.handlers.render_post.remove(ktx_render_handler)
 
 if __name__ == "__main__":
     register()
